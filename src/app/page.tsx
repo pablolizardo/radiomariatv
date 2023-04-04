014a7850c28c28559249a59479fa251d10c38a29
Pablo Lizardo <pablo.lizardo@blackboard.com>
2023-04-04 13:43:22 -0300

    seo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,9 @@ export default function Home() {
   return (
 
     <div className="video-crop">
-      <div className="video-container">
-        <iframe
-          allowFullScreen
-          src="https://videocloud.instream.audio:2000/VideoPlayer/radiomaria?html5ui=1&autoplay=1&muted=0" />
-      </div>
+      <iframe className="video-container"
+        allowFullScreen
+        src="https://videocloud.instream.audio:2000/VideoPlayer/radiomaria?html5ui=1&autoplay=1&muted=0" />
     </div>
   )
 }
