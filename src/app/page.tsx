09aae21a9449c1871182ce4126aa6f100231eb7b
Pablo Lizardo <pablo.lizardo@blackboard.com>
2023-04-05 11:36:05 -0300

    stream source

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="video-crop">
       <iframe className="video-container"
         allowFullScreen
-        // src="https://videocloud.instream.audio:2000/VideoPlayer/radiomaria?html5ui=1&autoplay=1&muted=0" 
+        src="https://videocloud.instream.audio:2000/VideoPlayer/radiomaria?html5ui=1&autoplay=1&muted=0" 
         />
     </div>
     <Footer />
